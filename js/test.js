5310e6cfa46e669f35eb64eaa9d64723ef7c244d
Mateusz Wegrzyn <matiice@interia.pl>
2017-04-14 18:13:10 +0200

    dzwieki

--- js/test.js
+++ js/test.js
@@ -1,18 +1,43 @@
 $(function () {
 
             'use strict'
-
+          
             $("button").click(function () {
                 $(this).fadeOut();
+/*= PRZYPADEK==*/
+                
+                var textpar = $('').text();
 
-                var textpar = $('#napis').text();
-
-
+/*shotgun*/
+                $( "#box" ).click(function() {
+                var audio = new Audio("/img/shotgun.mp3"); audio.play();   
+});
+                          
+                
+                
+                
+                
+/*KACZKA CLICK*/
                 $('#kwadrat').on({
                     'click': function () {
-                        $('.napis').text(textpar + ' WESOŁYCH ŚWIĄT ').fadeOut(15000);
+                  
+                        var score = parseInt(document.getElementById("score").innerHTML); score++;
+                        document.getElementById("score").innerHTML = score;
+                        
+                        var audio2 = new Audio("/img/hug.mp3"); audio2.play();
+/* jesli 5 pkt to...*/                        
+                        if (score >= 5) {
+                        
+                            $('.napis').text(textpar + ' WYGRANA! WESOŁYCH ŚWIĄT ').fadeOut(15000);
+                        
+                            var audio3 = new Audio("/img/victory.mp3"); audio3.play();
+                            
+                            };
+
 
                     },
+                    
+/*KACZKA UCIEKAJ*/                    
                     'mouseover': function () {
                         var rand = Math.floor((Math.random() * 100));
                         var rand2 = Math.floor((Math.random() * 100));
@@ -22,7 +47,6 @@ $(function () {
                             'background-color': 'green',
                             duration: 30});
                         
-        /* PONIŻEJ TĄ FUNKCJĘ NAPISAŁEM SAM!!! JESTEM MEGA DUMNY */
                         if (rand <=50){
                             $("#kwadrat").css("background-image", "url(/img/d-jpg/2.png)");
                         }else{
@@ -32,10 +56,22 @@ $(function () {
                     });
 
                 });
-            });
-  
+            
+  });
+
+/*document.getElementById('kwadrat').onclick=function(){
+    
+}*/
+    
 
 
+/*    if (score <= 4) {
+        function () {
+                        $('.napis').text(textpar + ' WESOŁYCH ŚWIĄT ').fadeOut(15000);
+            
+    }else{ $funcion()};
+
+  */  
 /*setInterval (function() {
     var ptak = ["img/d-jpg/1.jpg", "img/d-jpg/2.jpg"];
      document.getElementById("#kwadrat").style.backgroundImage ="url(" + tla[Math.floor(Math.random() * 2)] + ")";
